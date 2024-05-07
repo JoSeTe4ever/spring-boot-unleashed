3d68171337adeb90f3ed8089872d153501aac23a
Jose Herminio Parreno Piqueras <josete4ever@gmail.com>
2024-05-07 19:57:53 +0200

    Initial commit
    This is the initial commit, just scaffold code

---
+++ src/main/java/com/jopisoft/unleashed/UnleashedApplication.java
@@ -0,0 +1,13 @@
+package com.jopisoft.unleashed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnleashedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnleashedApplication.class, args);
+	}
+
+}
