b1abc1f0c85f2b943c6e3d14c6ece9fa3fb435f4
Jose Herminio Parreno Piqueras <josete4ever@gmail.com>
2024-05-16 23:13:32 +0200

    fix: now is scanning properly

--- src/main/java/com/jopisoft/unleashed/UnleashedConfig.java
+++ src/main/java/com/jopisoft/unleashed/UnleashedConfig.java
@@ -1,13 +1,16 @@
+package com.jopisoft.unleashed;
+
+import java.util.Arrays;
+
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UnleashedConfig {
     
-
     public UnleashedConfig(ApplicationContext applicationContext) {
         System.out.println("UnleashedConfig created");
-        applicationContext.getBe
-        //
+        //Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(System.out::println);
     }
 
 }
